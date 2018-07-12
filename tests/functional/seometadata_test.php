8c1a9ef9f9fec03e2ce20f874088cc0cb69df062
Alfredo Ramos <alfredo.ramos@yandex.com>
2018-07-11 22:26:01 -0500

    Add ACP functional tests

--- tests/functional/seometadata_test.php
+++ tests/functional/seometadata_test.php
@@ -28,7 +28,10 @@ class seometadata_test extends phpbb_functional_test_case
 		$db = $this->get_db();
 		$sql = 'UPDATE ' . CONFIG_TABLE . '
 			SET  ' . $db->sql_build_array('UPDATE',
-				['config_value' => 'seo/default_image.png']
+				[
+					'config_value' => 'seo/default_image.png',
+					'is_dynamic' => 1 // Fix cache
+				]
 			) . '
 			WHERE ' . $db->sql_build_array('UPDATE',
 				['config_name' => 'seo_metadata_default_image']
@@ -36,8 +39,6 @@ class seometadata_test extends phpbb_functional_test_case
 		$db->sql_query($sql);
 		$db->sql_close();
 		unset($db);
-
-		$this->login();
 	}
 
 	public function test_open_graph()
@@ -63,7 +64,7 @@ class seometadata_test extends phpbb_functional_test_case
 			$elements[$property] = $crawler->filter(
 				sprintf('meta[property="og:%s"]', $property)
 			);
-			//$this->assertSame(1, $elements[$property]->count());
+			$this->assertSame(1, $elements[$property]->count());
 		}
 
 		$this->assertSame(
@@ -90,9 +91,39 @@ class seometadata_test extends phpbb_functional_test_case
 			'http://localhost/viewtopic.php?t=1',
 			$elements['url']->attr('content')
 		);
-		/*$this->assertSame(
-			'http://localhost/phpBB3/images/seo/default_image.png',
+		$this->assertSame(
+			'http://localhost/images/seo/default_image.png',
 			$elements['image']->attr('content')
-		);*/
+		);
+	}
+
+	public function test_acp_form_settings()
+	{
+		$this->login();
+		$this->admin_login();
+
+		$crawler = self::request('GET', sprintf(
+			'adm/index.php?i=-alfredoramos-seometadata-acp-main_module&mode=settings&sid=%s',
+			$this->sid
+		));
+
+		$form = $crawler->selectButton($this->lang('SUBMIT'))->form();
+
+		$this->assertSame(1, $crawler->filter('#seo_metadata_settings')->count());
+
+		$this->assertTrue($form->has('seo_metadata_desc_length'));
+		$this->assertSame(160, (int) $form->get('seo_metadata_desc_length')->getValue());
+
+		$this->assertTrue($form->has('seo_metadata_desc_strategy'));
+		$this->assertSame(0, (int) $form->get('seo_metadata_desc_strategy')->getValue());
+
+		$this->assertTrue($form->has('seo_metadata_default_image'));
+		$this->assertSame('seo/default_image.png', $form->get('seo_metadata_default_image')->getValue());
+
+		$this->assertTrue($form->has('seo_metadata_open_graph'));
+		$this->assertSame(1, (int) $form->get('seo_metadata_open_graph')->getValue());
+
+		$this->assertTrue($form->has('seo_metadata_json_ld'));
+		$this->assertSame(1, (int) $form->get('seo_metadata_json_ld')->getValue());
 	}
 }
