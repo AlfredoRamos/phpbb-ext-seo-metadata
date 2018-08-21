422a3cff55d74498745f95999e69403b25834a6d
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2018-08-21 15:50:22 -0500

    Add image extractor functional tests

--- tests/functional/seometadata_test.php
+++ tests/functional/seometadata_test.php
@@ -27,20 +27,11 @@ class seometadata_test extends phpbb_functional_test_case
 	{
 		parent::setUp();
 
-		$db = $this->get_db();
-		$sql = 'UPDATE ' . CONFIG_TABLE . '
-			SET  ' . $db->sql_build_array('UPDATE',
-				[
-					'config_value' => 'seo/default_image.png',
-					'is_dynamic' => 1 // Fix cache
-				]
-			) . '
-			WHERE ' . $db->sql_build_array('UPDATE',
-				['config_name' => 'seo_metadata_default_image']
-			);
-		$db->sql_query($sql);
-		$db->sql_close();
-		unset($db);
+		// Set default image
+		$this->update_config_value(
+			'seo_metadata_default_image',
+			'seo/default_image.png'
+		);
 
 		$this->expected_data = [
 			'title' => 'Welcome to phpBB3',
@@ -48,6 +39,29 @@ class seometadata_test extends phpbb_functional_test_case
 			'url' => 'http://localhost/viewtopic.php?t=1',
 			'image' => 'http://localhost/images/seo/default_image.png'
 		];
+	}
+
+	protected function update_config_value($name = '', $value = '')
+	{
+		if (empty($name) || empty($value))
+		{
+			return;
+		}
+
+		$db = $this->get_db();
+		$sql = 'UPDATE ' . CONFIG_TABLE . '
+			SET  ' . $db->sql_build_array('UPDATE',
+				[
+					'config_value' => $value,
+					'is_dynamic' => 1 // Fix cache
+				]
+			) . '
+			WHERE ' . $db->sql_build_array('UPDATE',
+				['config_name' => $name]
+			);
+		$db->sql_query($sql);
+		$db->sql_close();
+		unset($db);
 	}
 
 	public function test_open_graph()
@@ -181,6 +195,9 @@ class seometadata_test extends phpbb_functional_test_case
 		$this->assertTrue($form->has('seo_metadata_desc_strategy'));
 		$this->assertSame(0, (int) $form->get('seo_metadata_desc_strategy')->getValue());
 
+		$this->assertTrue($form->has('seo_metadata_image_strategy'));
+		$this->assertSame(0, (int) $form->get('seo_metadata_image_strategy')->getValue());
+
 		$this->assertTrue($form->has('seo_metadata_default_image'));
 		$this->assertSame('seo/default_image.png', $form->get('seo_metadata_default_image')->getValue());
 
@@ -189,5 +206,101 @@ class seometadata_test extends phpbb_functional_test_case
 
 		$this->assertTrue($form->has('seo_metadata_json_ld'));
 		$this->assertSame(1, (int) $form->get('seo_metadata_json_ld')->getValue());
+	}
+
+	public function test_extracted_image_first_found()
+	{
+		$this->login();
+
+		$data = [
+			'title' => 'SEO Metadata Functional Test 1',
+			'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius augue ut enim eleifend varius id eget nisl. Suspendisse potenti. Vivamus fringilla tellus consequat lectus venenatis faucibus. Aliquam hendrerit eleifend turpis et ultrices. Cras accumsan, dui sollicitudin faucibus auctor, nunc tellus viverra ex, id viverra nulla enim eget neque. Praesent gravida magna vitae erat convallis dictum. Sed ante lacus, gravida et pharetra vel, porttitor non leo. Sed auctor dolor et ullamcorper consectetur. Ut leo lacus, aliquam at dui eget, convallis tempor sapien. Integer sed lectus quis augue ultricies maximus sit amet nec erat. Duis odio odio, tincidunt quis porta eget, vulputate at eros. Etiam bibendum fringilla libero, sed lobortis lorem placerat eget'.PHP_EOL.
+				'[img]https://dummyimage.com/250x250/fff/000.jpg[/img]'.PHP_EOL.
+				'[img]https://dummyimage.com/600x400/000/fff.png[/img]'
+		];
+
+		$post = $this->create_topic(
+			2,
+			$data['title'],
+			$data['body']
+		);
+
+		$crawler = self::request('GET', vsprintf(
+			'viewtopic.php?t=%d&sid=%s',
+			[
+				$post['topic_id'],
+				$this->sid
+			]
+		));
+
+		$image = 'https://dummyimage.com/250x250/fff/000.jpg';
+		$elements = [];
+
+		// Open Graph image
+		$elements['opengraph'] = $crawler->filter('meta[property="og:image"]');
+
+		// JSON-LD image
+		$elements['jsonld'] = $crawler->filter('script[type="application/ld+json"]');
+		$elements['jsonld'] = json_decode($elements['jsonld']->text(), true);
+
+		$this->assertFalse(empty($elements['opengraph']->attr('content')));
+		$this->assertSame(
+			$image,
+			$elements['opengraph']->attr('content')
+		);
+
+		$this->assertFalse(empty($elements['jsonld']['image']));
+		$this->assertSame(
+			$image,
+			$elements['jsonld']['image']
+		);
+	}
+
+	public function test_extracted_image_fallback()
+	{
+		$this->login();
+
+		$data = [
+			'title' => 'SEO Metadata Functional Test 2',
+			'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius augue ut enim eleifend varius id eget nisl. Suspendisse potenti. Vivamus fringilla tellus consequat lectus venenatis faucibus. Aliquam hendrerit eleifend turpis et ultrices. Cras accumsan, dui sollicitudin faucibus auctor, nunc tellus viverra ex, id viverra nulla enim eget neque. Praesent gravida magna vitae erat convallis dictum. Sed ante lacus, gravida et pharetra vel, porttitor non leo. Sed auctor dolor et ullamcorper consectetur. Ut leo lacus, aliquam at dui eget, convallis tempor sapien. Integer sed lectus quis augue ultricies maximus sit amet nec erat. Duis odio odio, tincidunt quis porta eget, vulputate at eros. Etiam bibendum fringilla libero, sed lobortis lorem placerat eget'.PHP_EOL.
+				'[img]https://dummyimage.com/150x150/fff/000.jpg[/img]'.PHP_EOL.
+				'[img]https://dummyimage.com/60x40/000/fff.png[/img]'
+		];
+
+		$post = $this->create_topic(
+			2,
+			$data['title'],
+			$data['body']
+		);
+
+		$crawler = self::request('GET', vsprintf(
+			'viewtopic.php?t=%d&sid=%s',
+			[
+				$post['topic_id'],
+				$this->sid
+			]
+		));
+
+		$image = 'http://localhost/images/seo/default_image.png';
+		$elements = [];
+
+		// Open Graph image
+		$elements['opengraph'] = $crawler->filter('meta[property="og:image"]');
+
+		// JSON-LD image
+		$elements['jsonld'] = $crawler->filter('script[type="application/ld+json"]');
+		$elements['jsonld'] = json_decode($elements['jsonld']->text(), true);
+
+		$this->assertFalse(empty($elements['opengraph']->attr('content')));
+		$this->assertSame(
+			$image,
+			$elements['opengraph']->attr('content')
+		);
+
+		$this->assertFalse(empty($elements['jsonld']['image']));
+		$this->assertSame(
+			$image,
+			$elements['jsonld']['image']
+		);
 	}
 }
