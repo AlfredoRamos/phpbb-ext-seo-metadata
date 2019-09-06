538b8969ee084be5a236ecfa0ef35dd3f039561e
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2019-09-06 15:28:46 -0500

    Use filesystem::clean_path() in helper::clean_image()
    Also sort and improve visualization of Facebook supported locales

--- includes/helper.php
+++ includes/helper.php
@@ -376,7 +376,7 @@ class helper
 						[
 							trim(mb_substr($description, 0, $desc_length, $encoding)),
 							$ellipsis
-						]
+							]
 					);
 				break;
 
@@ -433,7 +433,7 @@ class helper
 		// https://github.com/phpbb/phpbb/pull/5673
 		//
 		//$image_path = $this->filesystem->realpath($base_path . '/' . $uri);
-		$image_path = $this->get_absolute_path($base_path . '/' . $uri);
+		$image_path = $this->filesystem->clean_path($base_path . '/' . $uri);
 
 		// Avoid path traversal attack
 		// Image must exist and be readable
@@ -803,64 +803,30 @@ class helper
 	}
 
 	/**
-	 * Convert given path to absolute.
-	 *
-	 * Based on Sven Arduwie's work
-	 * https://www.php.net/manual/en/function.realpath.php#84012
-	 *
-	 * @param string $path
-	 *
-	 * @return string
-	 */
-	public function get_absolute_path($path = '')
-	{
-		if (empty($path))
-		{
-			return '';
-		}
-
-		$parts = array_filter(explode('/', $path), 'strlen');
-		$resolved = [];
-
-		foreach ($parts as $part)
-		{
-			if ($part === '.')
-			{
-				continue;
-			}
-			else if ($part === '..')
-			{
-				array_pop($resolved);
-			}
-			else
-			{
-				$resolved[] = $part;
-			}
-		}
-
-		// Generate absolute path
-		$absolute_path = implode('/', $resolved);
-
-		// Add leading slash
-		if (substr($absolute_path, 0, 1) !== '/')
-		{
-			$absolute_path = trim('/' . $absolute_path);
-		}
-
-		return $absolute_path;
-	}
-
-	/**
 	 * Supported Open Graph locales.
 	 *
 	 * https://developers.facebook.com/docs/internationalization
 	 * https://developers.facebook.com/docs/messenger-platform/messenger-profile/supported-locales/
 	 *
+	 * Last updated: 2019-09-06
+	 *
 	 * @return array
 	 */
 	private function supported_locales()
 	{
-		return ['en_US', 'ca_ES', 'cs_CZ', 'cx_PH', 'cy_GB', 'da_DK', 'de_DE', 'eu_ES', 'en_UD', 'es_LA', 'es_ES', 'gn_PY', 'fi_FI', 'fr_FR', 'gl_ES', 'hu_HU', 'it_IT', 'ja_JP', 'ko_KR', 'nb_NO', 'nn_NO', 'nl_NL', 'fy_NL', 'pl_PL', 'pt_BR', 'pt_PT', 'ro_RO', 'ru_RU', 'sk_SK', 'sl_SI', 'sv_SE', 'th_TH', 'tr_TR', 'ku_TR', 'zh_CN', 'zh_HK', 'zh_TW', 'af_ZA', 'sq_AL', 'hy_AM', 'az_AZ', 'be_BY', 'bn_IN', 'bs_BA', 'bg_BG', 'hr_HR', 'nl_BE', 'en_GB', 'et_EE', 'fo_FO', 'fr_CA', 'ka_GE', 'el_GR', 'gu_IN', 'hi_IN', 'is_IS', 'id_ID', 'ga_IE', 'jv_ID', 'kn_IN', 'kk_KZ', 'lv_LV', 'lt_LT', 'mk_MK', 'mg_MG', 'ms_MY', 'mt_MT', 'mr_IN', 'mn_MN', 'ne_NP', 'pa_IN', 'sr_RS', 'so_SO', 'sw_KE', 'tl_PH', 'ta_IN', 'te_IN', 'ml_IN', 'uk_UA', 'uz_UZ', 'vi_VN', 'km_KH', 'tg_TJ', 'ar_AR', 'he_IL', 'ur_PK', 'fa_IR', 'ps_AF', 'my_MM', 'qz_MM', 'or_IN', 'si_LK', 'rw_RW', 'cb_IQ', 'ha_NG', 'ja_KS', 'br_FR', 'tz_MA', 'co_FR', 'as_IN', 'ff_NG', 'sc_IT', 'sz_PL'];
+		return [
+			'af_ZA','ar_AR','as_IN','az_AZ','be_BY','bg_BG','bn_IN','br_FR','bs_BA','ca_ES',
+			'cb_IQ','co_FR','cs_CZ','cx_PH','cy_GB','da_DK','de_DE','el_GR','en_GB','en_UD',
+			'en_US','es_ES','es_LA','et_EE','eu_ES','fa_IR','ff_NG','fi_FI','fo_FO','fr_CA',
+			'fr_FR','fy_NL','ga_IE','gl_ES','gn_PY','gu_IN','ha_NG','he_IL','hi_IN','hr_HR',
+			'hu_HU','hy_AM','id_ID','is_IS','it_IT','ja_JP','ja_KS','jv_ID','ka_GE','kk_KZ',
+			'km_KH','kn_IN','ko_KR','ku_TR','lt_LT','lv_LV','mg_MG','mk_MK','ml_IN','mn_MN',
+			'mr_IN','ms_MY','mt_MT','my_MM','nb_NO','ne_NP','nl_BE','nl_NL','nn_NO','or_IN',
+			'pa_IN','pl_PL','ps_AF','pt_BR','pt_PT','qz_MM','ro_RO','ru_RU','rw_RW','sc_IT',
+			'si_LK','sk_SK','sl_SI','so_SO','sq_AL','sr_RS','sv_SE','sw_KE','sz_PL','ta_IN',
+			'te_IN','tg_TJ','th_TH','tl_PH','tr_TR','tz_MA','uk_UA','ur_PK','uz_UZ','vi_VN',
+			'zh_CN','zh_HK','zh_TW'
+		];
 	}
 
 	/**
