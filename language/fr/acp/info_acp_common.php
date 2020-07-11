e16e961dad7804a7646a35ba8fafde0350fec0bc
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2020-07-11 15:47:03 -0500

    New translations info_acp_common.php (French)

---
+++ language/fr/acp/info_acp_common.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * SEO Metadata extension for phpBB.
+ * @author Alfredo Ramos <alfredo.ramos@yandex.com>
+ * @copyright 2018 Alfredo Ramos
+ * @license GNU GPL-2.0-only
+ */
+
+/**
+ * @ignore
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+ * @ignore
+ */
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+$lang = array_merge($lang, [
+	'ACP_SEO_METADATA' => 'SEO Metadata',
+	'LOG_SEO_METADATA_DATA' => '<strong>Les données de SEO Metadata ont été modifiées</strong><br>» %s'
+]);
